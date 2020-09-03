c485dc32260aeb633d80243f874687ac1202d867
Yuki Ito <mrno110y@gmail.com>
2020-09-03 16:28:48 +0900

    add test cases

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,11 +1,21 @@
 package a
 
+import "errors"
+
+type myinterface interface{}
+
+type myerr struct{}
+
+func (e *myerr) Error() string {
+	return "myerr"
+}
+
 func f() (int, error) {
 	return 0, nil
 }
 
 var (
-	_, _ = f() // want "receiving error with _"
+	_, _    = f() // want "receiving error with _"
 	_, err1 = f() // OK
 )
 
@@ -22,6 +32,22 @@ func main() {
 	a, _ = 12, error(nil) // want "receiving error with _"
 
 	_, _ = 12, "foo" // OK
+
+	m := map[string]error{
+		"error1": errors.New("error1"),
+	}
+
+	_, ok := m["error1"] // want "receiving error with _"
+	if !ok {
+	}
+
+	_ = &myerr{} // want "receiving error with _"
+
+	var i myinterface
+
+	_, ok = i.(error) // want "receiving error with _"
+	if !ok {
+	}
 
 	print(a)
 }
